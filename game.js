b726d7111f1b0d05bb94a88632b5866e0db15f4b
SantiagoLaraE <oscarescobar773@gmail.com>
2023-08-18 14:19:50 -0500

    Tamaños de canvas y elementos

--- game.js
+++ game.js
@@ -2,13 +2,19 @@ const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
 function startGame() {
-    game.fillRect(0, 0, 100, 100);
-    game.clearRect(0, 0, 100, 20);
-
-  game.font = '16px Verdana, sans-serif' 
-  game.textAlign = 'start'
-  game.textBaseline = 'top'
-  game.fillText("My Text", 0, 0);
+  let canvaSize =
+    (window.innerWidth > window.innerHeight
+      ? window.innerHeight
+      : window.innerWidth) * 0.8;
+  canvas.setAttribute("width", canvaSize);
+  canvas.setAttribute("height", canvaSize);
+  const elementsSize = canvaSize / 10;
+  game.textAlign = "left";
+  game.textBaseline = "top";
+  game.font = `${elementsSize}px Verdana`;
+  for (let i = 0; i < 10; i++) {
+    game.fillText(emojis["X"], elementsSize * i, 0);
+  }
 }
 
 window.addEventListener("load", startGame);
