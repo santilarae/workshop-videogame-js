ac52064a85864afe075bad54966ed0140966fd97
SantiagoLaraE <oscarescobar773@gmail.com>
2023-08-18 18:47:23 -0500

    Eventos de movimiento

--- game.js
+++ game.js
@@ -1,8 +1,30 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
+const btnUp = document.querySelector("button#up");
+const btnDown = document.querySelector("button#down");
+const btnLeft = document.querySelector("button#left");
+const btnRight = document.querySelector("button#right");
 
 let canvaSize;
 let elementSize;
+
+const moveUp = () => console.log("Arriba");
+const moveDown = () => console.log("Abajo");
+const moveLeft = () => console.log("Izquierda");
+const moveRight = () => console.log("Derecha");
+
+const moveByKey = (e) => {
+  if (e.key === "ArrowUp") moveUp();
+  if (e.key === "ArrowDown") moveDown();
+  if (e.key === "ArrowLeft") moveLeft();
+  if (e.key === "ArrowRight") moveRight();
+};
+
+btnUp.addEventListener("click", moveUp);
+btnDown.addEventListener("click", moveDown);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+window.addEventListener("keyup", moveByKey);
 
 function startGame() {
   const mapArr = maps[0].match(/[IXO\-]+/g).map((col) => col.split(""));
