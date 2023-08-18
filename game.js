43626adee1d460d0a5b68493ce7838380b568774
SantiagoLaraE <oscarescobar773@gmail.com>
2023-08-18 13:38:18 -0500

    Intro canvas

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-console.log(maps);
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+
+function startGame() {
+    game.fillRect(0, 0, 100, 100);
+    game.clearRect(0, 0, 100, 20);
+
+  game.font = '16px Verdana, sans-serif' 
+  game.textAlign = 'start'
+  game.textBaseline = 'top'
+  game.fillText("My Text", 0, 0);
+}
+
+window.addEventListener("load", startGame);
